6a1f708ca979991d9e9134efd779fb9f3e111dbd
LakhanRaut <lakhanraut24@gmail.com>
2023-12-29 21:08:58 +0530

    sign-out working

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,14 @@
 import Container from "@/components/Container";
 import ProfileInfo from "@/components/ProfileInfo";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const session = await getServerSession();
+  if (!session || !session.user) {
+    redirect("/");
+  }
   return (
     <Container>
       <p className="text-xl font-semibold pb-10 underline underline-offset-4 decoration-[1px]">
